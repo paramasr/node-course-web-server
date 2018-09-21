140a8496df4f1e00a3e25a70be4c6f4de8f24719
Rupa <rupa78@gmail.com>
2018-09-21 14:53:07 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname +'/public'));
+
+app.use( (req, res, next) => {
+	var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	
+	fs.appendFile('server.log', log + '\r\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log')
+		}
+	});
+	next();
+});
+
+app.use( (req, res, next) => {
+	res.render('maintenance.hbs');
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+
+app.get('/', (request, response) => {
+	//response.send('<h1>Hello Express!</h1>');
+	response.render('Home.hbs', {
+		title: 'Home Page',
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my first webpage!'
+	});
+});
+app.get('/about', (request, response) => {
+	response.render('About.hbs', {
+		title: 'About Page',
+		pageTitle: 'About Page'
+	});
+})
+app.get('/bad', (request, response) => {
+	response.send({
+		error: 'function failed',
+		code: 'info'
+	});
+})
+app.listen(3000, () => {
+	console.log("Server is up on port 3000");
+});
